3657534f0dfd6167193a5c8e7d95b4c6a5d2470c
Rebecca Smith <70491756+rsmith1220@users.noreply.github.com>
2023-08-08 20:32:03 -0600

    some changes
    still trying

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,8 +1,25 @@
 package hello;
 
+import org.jivesoftware.smack.*;
+import org.jivesoftware.smack.chat.Chat;
+import org.jivesoftware.smack.chat.ChatManager;
+import org.jivesoftware.smack.chat.ChatMessageListener;
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 public class HelloWorld {
   public static void main(String[] args) {
-    Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
+    String username = "1220";
+    String password = "122001";
+    // String recipient = "recipient_username";
+    String server = "alumchat.xyz";
+
+    XMPPTCPConnectionConfiguration config = XMPPTCPConnectionConfiguration.builder()
+            .setUsernameAndPassword(username, password)
+            .setXmppDomain(server)
+            .build();
+
+    AbstractXMPPConnection connection = new XMPPTCPConnection(config);
+
+
   }
 }
