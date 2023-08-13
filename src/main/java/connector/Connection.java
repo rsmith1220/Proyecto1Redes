1bdbf896f31199b3e6869eab95b1cc9e0f2e7934
Rebecca Smith <70491756+rsmith1220@users.noreply.github.com>
2023-08-13 16:04:16 -0600

    messages can be sent and user login

--- src/main/java/connector/Connection.java
+++ src/main/java/connector/Connection.java
@@ -1,6 +1,8 @@
 package connector;
 
 import org.jivesoftware.smack.*;
+import org.jivesoftware.smack.chat2.Chat;
+import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 import org.jxmpp.jid.EntityBareJid;
@@ -10,8 +12,17 @@ import org.sakaiproject.chat2.model.ChatManager;
 
 import java.io.IOException;
 
+import java.util.*;
+
 public class Connection {
     public static void main(String[] args) throws SmackException, IOException, XMPPException, InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre de usuario");
+        String user = sc.nextLine();
+        System.out.println("Ingrese su password");
+        String pass = sc.nextLine();
 
         try {
             XMPPTCPConnectionConfiguration config = XMPPTCPConnectionConfiguration.builder()
@@ -19,7 +30,7 @@ public class Connection {
                     .setXmppDomain("alumchat.xyz")
                     .setPort(5222)
                     .setSecurityMode(ConnectionConfiguration.SecurityMode.disabled)
-                    .setUsernameAndPassword("rebecca", "122001")
+                    .setUsernameAndPassword(user, pass)
                     .build();
 
             AbstractXMPPConnection connection = new XMPPTCPConnection(config);
@@ -27,6 +38,14 @@ public class Connection {
             connection.login(); // Logs in
 
             System.out.println("Connected");
+
+            org.jivesoftware.smack.chat2.ChatManager chat = org.jivesoftware.smack.chat2.ChatManager
+                    .getInstanceFor(connection);
+            EntityBareJid jid = JidCreate.entityBareFrom("ABCD@alumchat.xyz");
+            // Chat chat = chatManager.chatWith(jid);
+            Chat chat2 = chat.chatWith(jid);
+            chat2.send("Hello!");
+
         } catch (Exception e) {
             System.out.println(e);
         }
