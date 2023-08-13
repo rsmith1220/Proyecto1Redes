ca8a81efe3834aaf18ca5d86be084b4a71f38fed
Rebecca Smith <70491756+rsmith1220@users.noreply.github.com>
2023-08-13 15:40:19 -0600

    connection working

--- src/main/java/connector/Connection.java
+++ src/main/java/connector/Connection.java
@@ -1,35 +1,35 @@
 package connector;
+
 import org.jivesoftware.smack.*;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 import org.jxmpp.jid.EntityBareJid;
 import org.jxmpp.jid.impl.JidCreate;
 import org.jxmpp.stringprep.XmppStringprepException; // Import the exception
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.KeyManagementException;
+import org.sakaiproject.chat2.model.ChatManager;
 
 import java.io.IOException;
 
 public class Connection {
     public static void main(String[] args) throws SmackException, IOException, XMPPException, InterruptedException {
-        XMPPTCPConnectionConfiguration config = XMPPTCPConnectionConfiguration.builder()
+
+        try {
+            XMPPTCPConnectionConfiguration config = XMPPTCPConnectionConfiguration.builder()
                     .setHost("alumchat.xyz")
                     .setXmppDomain("alumchat.xyz")
                     .setPort(5222)
                     .setSecurityMode(ConnectionConfiguration.SecurityMode.disabled)
                     .setUsernameAndPassword("rebecca", "122001")
                     .build();
-             
 
+            AbstractXMPPConnection connection = new XMPPTCPConnection(config);
+            connection.connect(); // Establishes a connection to the server
+            connection.login(); // Logs in
 
-  AbstractXMPPConnection connection = new XMPPTCPConnection(config);
-connection.connect(); //Establishes a connection to the server
-connection.login(); //Logs in
+            System.out.println("Connected");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
     }
 }
-
